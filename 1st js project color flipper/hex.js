2904784b628abf63c734e26d3869bcc6e5d3b3c2
Hrithik <hrithik00g@gmail.com>
2021-06-14 12:09:35 +0530

    hex color code generated randomly hex-js

--- 1st js project color flipper/hex.js
+++ 1st js project color flipper/hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  // random num generated is added and assigned to hexColor
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber() {
+  // get random numbers between the array length of hex[ 0 - "F"]
+  return Math.floor(Math.random() * hex.length);
+}
