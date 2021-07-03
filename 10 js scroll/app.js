0bbe3b22e8a1aa3d764e59d4d3adddabaeb3a147
Hrithik <hrithik00g@gmail.com>
2021-07-03 20:23:26 +0530

    added nav menu to 10js scroll project

--- 10 js scroll/app.js
+++ 10 js scroll/app.js
@@ -5,8 +5,24 @@
 
 // ********** set date ************
 
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
 // ********** close links ************
 
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+const navToggle = document.querySelector(".nav-toggle");
+
+navToggle.addEventListener("click", function () {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+      linksContainer.style.height = 0;
+  }
+});
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
